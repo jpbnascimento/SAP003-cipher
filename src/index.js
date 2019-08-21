96562073a9fb686c09fbc89042904bb745920a5e
Jessica Patricia <jessicapatricia03@hotmail.com>
2019-08-21 13:58:10 -0300

    integração do html com js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function clique(){
+  
+    let frase = document.getElementById("cd").value;
+    let deslocamento = document.getElementById("des").value;
+    console.log(frase);
+    console.log(deslocamento);
+}
+
