6b3266e22f1b50378fb66396950bfeec37a60457
Jessica Patricia <jessicapatricia03@hotmail.com>
2019-08-26 14:18:16 -0300

    codificar e decodificar minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -3,16 +3,36 @@ window.cipher = {encode: cifrar, decode: decifrar};
 function cifrar(text, deslocamento){
   let resultado = "";
   for (let i=0; i < text.length; i++){
+    if (text.charCodeAt(i)>=65 && text.charCodeAt(i)<=90){
       resultado += String.fromCharCode((text.charCodeAt(i) - 65 + deslocamento) % 26 + 65);
   }
-  
+    else if (text.charCodeAt(i)>=97 && text.charCodeAt(i)<=122){
+      resultado += String.fromCharCode((text.charCodeAt(i) - 97 + deslocamento) % 26 + 97);
+  }
+    else{
+      resultado = charCodeAt(i);
+    }
+    
+  }
+   
   console.log(resultado);
 }
 
 function decifrar(text, deslocamento){
   let resultado = "";
+  
   for (let i=0; i < text.length; i++){
+
+    if (text.charCodeAt(i)>=65 && text.charCodeAt(i)<=90){
       resultado += String.fromCharCode((text.charCodeAt(i) - 90 - deslocamento) % 26 + 90);
+  }
+    else if (text.charCodeAt(i)>=97 && text.charCodeAt(i)<=122){
+      resultado += String.fromCharCode((text.charCodeAt(i) - 122 - deslocamento) % 26 + 122);
+  }
+    else {
+      resultado = charCodeAt(i);
+
+    }
   }
   console.log(resultado);
 }
