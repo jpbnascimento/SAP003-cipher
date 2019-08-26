9b2afea443cacee18ffc6995eedac834e724f3ef
Jessica Patricia <jessicapatricia03@hotmail.com>
2019-08-26 16:00:08 -0300

    correção de erros

--- src/cipher.js
+++ src/cipher.js
@@ -1,37 +1,29 @@
 window.cipher = {encode: cifrar, decode: decifrar};
 
-function cifrar(text, deslocamento){
+function cifrar(deslocamento, text) {
   let resultado = "";
-  for (let i=0; i < text.length; i++){
-    if (text.charCodeAt(i)>=65 && text.charCodeAt(i)<=90){
+  for (let i=0; i < text.length; i++) {
+    if (text.charCodeAt(i)>=65 && text.charCodeAt(i)<=90) {
       resultado += String.fromCharCode((text.charCodeAt(i) - 65 + deslocamento) % 26 + 65);
-  }
-    else if (text.charCodeAt(i)>=97 && text.charCodeAt(i)<=122){
-      resultado += String.fromCharCode((text.charCodeAt(i) - 97 + deslocamento) % 26 + 97);
-  }
-    else{
-      resultado = charCodeAt(i);
     }
-    
+    else if (text.charCodeAt(i)>=97 && text.charCodeAt(i)<=122) {
+      resultado += String.fromCharCode((text.charCodeAt(i) - 97 + deslocamento) % 26 + 97);
+    }    
   }
    
   console.log(resultado);
 }
 
-function decifrar(text, deslocamento){
+function decifrar(deslocamento, text) {
   let resultado = "";
   
-  for (let i=0; i < text.length; i++){
+  for (let i=0; i < text.length; i++) {
 
-    if (text.charCodeAt(i)>=65 && text.charCodeAt(i)<=90){
+    if (text.charCodeAt(i)>=65 && text.charCodeAt(i)<=90) {
       resultado += String.fromCharCode((text.charCodeAt(i) - 90 - deslocamento) % 26 + 90);
-  }
-    else if (text.charCodeAt(i)>=97 && text.charCodeAt(i)<=122){
+    }
+    else if (text.charCodeAt(i)>=97 && text.charCodeAt(i)<=122) {
       resultado += String.fromCharCode((text.charCodeAt(i) - 122 - deslocamento) % 26 + 122);
-  }
-    else {
-      resultado = charCodeAt(i);
-
     }
   }
   console.log(resultado);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-function pegaValorCifrar(){
-    let text = document.getElementById("cd").value;
-    let deslocamento = Number(document.getElementById("des").value);
-    document.getElementById("retorno").innerHTML= `Seu codigo cifrado é ${window.cipher.encode(text, deslocamento)} `;
+function pegaValorCifrar() {
+  let text = document.getElementById("cd").value;
+  let deslocamento = Number(document.getElementById("des").value);
+  document.getElementById("retorno").innerHTML= `Seu codigo cifrado é ${window.cipher.encode(text, deslocamento)} `;
 }
 
-function pegaValorDecifrar(){
-    let text = document.getElementById("cd").value;
-    let deslocamento = Number(document.getElementById("des").value);
-    document.getElementById("retorno").innerHTML= `Seu codigo decifrado é ${window.cipher.decode(text, deslocamento)} `;
+function pegaValorDecifrar() {
+  let text = document.getElementById("cd").value;
+  let deslocamento = Number(document.getElementById("des").value);
+  document.getElementById("retorno").innerHTML= `Seu codigo decifrado é ${window.cipher.decode(text, deslocamento)} `;
 }
-
-
